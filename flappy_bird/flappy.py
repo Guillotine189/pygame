f9a73702635df45922280443d6dd4026cb63195c
sarthaksinghal@pm.me <sarthaksinghal@pm.me>
2023-05-10 05:16:09 +0530

    added buttons

--- flappy_bird/flappy.py
+++ flappy_bird/flappy.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 pygame.font.init()
 pygame.init()
@@ -8,6 +10,8 @@ SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 player_image = pygame.image.load('./images/images.png')
+p1 = pygame.image.load('./images/top.png')
+p2 = pygame.image.load('./images/down.png')
 menu_bg = pygame.image.load('./images/manu_1400x800.png')
 
 upper_limit = SCREEN_HEIGHT - 154
@@ -15,19 +19,49 @@ lower_limit = -50
 
 text = pygame.font.SysFont("monospace", 50)
 
-def player(x,y):
+class Button:
+    def __init__(self,x,y,text):
+        self.x = x
+        self.y = y
+        self.text = text
+        self.draw()
+    def draw(self):
+        button_text = text.render(self.text, 1, 'black')
+        button_rect = pygame.rect.Rect((self.x, self.y), (150, 50))
+        if self.check_clicekd():
+            pygame.draw.rect(screen, 'dark grey', button_rect, 0, 5)
+        else:
+            pygame.draw.rect(screen, 'light grey', button_rect, 0, 5)
 
-    screen.blit(player_image, (x, y))
+        screen.blit(button_text, (self.x + 15, self.y + 2))
+
+
+    def check_clicekd(self):
+        m_pos = pygame.mouse.get_pos()
+        left_cl = pygame.mouse.get_pressed()[0]
+        button_rect = pygame.rect.Rect((self.x, self.y), (150, 50))
+        if left_cl and button_rect.collidepoint(m_pos):
+            return True
+        else:
+            return False
+
+def player(z, x, y):
+
+    screen.blit(z, (x, y))
 
 def main_menu():
     pygame.display.set_caption("MENU")
 
     textbgd = text.render("MENU", 1, (255, 255, 255))
+
     while True:
 
         screen.fill("black")
         screen.blit(menu_bg, (0, 0))
-        screen.blit(textbgd, (900,100))
+        screen.blit(textbgd, (900,300))
+
+        button1 = Button(800, 400, "PLAY")
+        button2 = Button(1000, 400, "EXIT")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -36,10 +70,17 @@ def main_menu():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     screen2()
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button1.check_clicekd():
+                    screen2()
+                if button2.check_clicekd():
+                    pygame.quit()
 
         playerX = 0
         playerY = 200
-        player(playerX, playerY)
+        player(player_image,playerX, playerY)
         pygame.display.update()
 
 
@@ -66,8 +107,13 @@ def screen2():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     playerY_ch -= up_speed
-                if event.key == pygame.K_BACKSPACE:
+                if event.key == pygame.K_BACKSPACE or event.key == pygame.K_ESCAPE:
                     main_menu()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    playerY_ch -= up_speed
+
 
         playerY += playerY_ch
         playerX += playerX_ch
@@ -82,7 +128,7 @@ def screen2():
             playerY_ch += gravity
 
 
-        player(playerX, playerY)
+        player(player_image, playerX, playerY)
         pygame.display.update()
 
 main_menu()
