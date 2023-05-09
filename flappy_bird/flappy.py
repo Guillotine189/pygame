31c6e34267651f7dedce2dd11045882c49596d0d
sarthaksinghal@pm.me <sarthaksinghal@pm.me>
2023-05-10 03:49:57 +0530

    added menu

--- flappy_bird/flappy.py
+++ flappy_bird/flappy.py
@@ -5,53 +5,78 @@ pygame.init()
 SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-pygame.display.set_caption("FLAPPY BIRD")
-background = pygame.image.load("./images/flappy_1400x800.png")
 player_image = pygame.image.load('./images/images.png')
-
-playerX = 0
-playerY = 200
-playerX_ch = 0
-playerY_ch = 0
-up_speed = 7
-gravity = 0.1
-
+menu_bg = pygame.image.load('./images/manu_1400x800.png')
+upper_limit = SCREEN_HEIGHT - 154
+lower_limit = -50
 def player(x,y):
+
     screen.blit(player_image, (x, y))
 
+def main_menu():
 
-running = True
-while running:
-
-    screen.fill((0, 0, 0))
-    screen.blit(background, (0, 0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    pygame.display.set_caption("MENU")
 
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                playerY_ch -= up_speed
+    while True:
+
+        screen.fill("black")
+        screen.blit(menu_bg, (0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    screen2()
+
+        playerX = 0
+        playerY = 200
+        player(playerX, playerY)
+        pygame.display.update()
 
 
+def screen2():
+    up_speed = 7
+    gravity = 0.1
+    pygame.display.set_caption("FLAPPY BIRD")
+    background = pygame.image.load("./images/flappy_1400x800.png")
 
+    playerX = 0
+    playerY = 200
+    playerX_ch = 0
+    playerY_ch = 0
 
+    while True:
 
-    playerY += playerY_ch
-    playerX += playerX_ch
-    playerY_ch += gravity
+        screen.fill("black")
+        screen.blit(background, (0, 0))
 
-    if playerY >= SCREEN_HEIGHT - 154:
-        playerY = SCREEN_HEIGHT - 154
-        playerY_ch -= gravity
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
-    if playerY < 0:
-        playerY = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playerY_ch -= up_speed
+                if event.key == pygame.K_BACKSPACE:
+                    main_menu()
+
+        playerY += playerY_ch
+        playerX += playerX_ch
         playerY_ch += gravity
 
+        if playerY >= upper_limit:
+            playerY = upper_limit
+            playerY_ch -= gravity
 
-    player(playerX, playerY)
-    pygame.display.update()
+        if playerY < lower_limit:
+            playerY = lower_limit
+            playerY_ch += gravity
+
+
+        player(playerX, playerY)
+        pygame.display.update()
+
+main_menu()
