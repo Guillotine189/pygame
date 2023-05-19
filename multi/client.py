02ef8c6e003443259d18497fd14c7ecbc75a2b6b
sarthaksinghal@pm.me <sarthaksinghal@pm.me>
2023-05-19 11:03:05 +0530

    user can disconnect and send multiple messages

--- multi/client.py
+++ multi/client.py
@@ -5,9 +5,7 @@ import threading
 import socket
 
 HOST = '127.0.0.1'
-PORT = 9101
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+PORT = 9102
 
 pygame.init()
 pygame.font.init()
@@ -16,57 +14,80 @@ WIDTH = 1400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 font_ = pygame.font.SysFont('monospace', 50)
-text = font_.render('CLICK TO CONNECT', 1, 'white')
+text = font_.render('CLICK TO CONNECT', True, 'white')
 text_rec = text.get_rect()
-text2 = font_.render('CONNECTED!', 1, 'white')
+text2 = font_.render('CONNECTED!', True, 'white')
 text2_rec = text.get_rect()
-text3 = font_.render('SEND MESSAGE', 1, 'white')
+text3 = font_.render('SEND MESSAGE', True, 'white')
 text3_rec = text.get_rect()
 
-text_rec.center = (WIDTH/2, HEIGHT/2)
-text2_rec.center = (WIDTH/2, HEIGHT/2)
-text3_rec.center = (WIDTH/2, HEIGHT/2 + 100)
+text_rec.center = (WIDTH / 2, HEIGHT / 2)
+text2_rec.center = (WIDTH / 2, HEIGHT / 2)
+text3_rec.center = (WIDTH / 2, HEIGHT / 2 + 100)
+connected = False
 
 
-def broadcast(message):
+def receive(client):
+    global connected
+    while connected:
+        try:
+            msg = client.recv(1024).decode()
+            if msg == "!D":
+                disconnected_text = font_.render("DISCONNECTED FROM SERVER!", True, 'white')
+                screen.blit(disconnected_text, (WIDTH/2 - 200, HEIGHT/2))
+                client.close()
+                connected = False
+        except:
+            client.close()
+            connected = False
+
+
+def broadcast(message, client):
     client.send(message.encode())
 
 
-def third():
-    third_run = True
-    user_input = ''
+def third(client):
+    global connected
+    user_input = ""
     enter_text = font_.render("ENTER TEXT", True, 'white')
     enter_text_rect = enter_text.get_rect()
-    enter_text_rect.center = (WIDTH/2, HEIGHT/2 - 200)
-    while third_run:
+    enter_text_rect.center = (WIDTH / 2, HEIGHT / 2 - 200)
+
+    while connected:
         screen.fill('black')
+
         user_text = font_.render(user_input, True, 'White')
         user_text_rect = user_text.get_rect()
-        user_text_rect.center = (WIDTH/2, HEIGHT/2)
+        user_text_rect.center = (WIDTH / 2, HEIGHT / 2)
         screen.blit(user_text, user_text_rect)
         pygame.draw.rect(screen, 'white', user_text_rect, 1)
         screen.blit(enter_text, enter_text_rect)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                client.send('!D'.encode())
+                client.close()
+                connected = False
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    broadcast(user_input)
-                    third_run = False
                 if event.key == pygame.K_BACKSPACE:
-                    user_input = user_input[0:-1]
+                    user_input = user_input[:-1]
+                elif event.key == pygame.K_RETURN:
+                    broadcast(user_input, client)
+                    user_input = ''
+                elif event.key == pygame.K_ESCAPE:
+                    conn(client)
                 else:
                     user_input += event.unicode
 
         pygame.display.update()
 
 
-def conn():
+def conn(client):
+    global connected
     screen.fill('black')
-    run = True
-    while run:
+    while connected:
         screen.blit(text2, text2_rec)
         screen.blit(text3, text3_rec)
         pygame.draw.rect(screen, 'white', text3_rec, 1)
@@ -75,22 +96,34 @@ def conn():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                client.send("!D".encode())
+                connected = False
+                client.close()
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    client.send("!D".encode())
+                    client.close()
+                    connected = False
+                    start()
             if event.type == pygame.MOUSEBUTTONDOWN:
-               if text3_rec.collidepoint(mpos):
-                    third()
-                    run = False
+                if text3_rec.collidepoint(mpos):
+                    third(client)
 
         pygame.display.update()
 
+
 def start():
-    global client, HOST, PORT
+    global screen, HOST, PORT, connected
     pygame.display.set_caption('TEST')
-    start_run = True
-    while start_run:
+
+    tee2 = 'NOT CONNECTED'
+    not_active_text = font_.render(tee2, True, 'white')
+    while True:
         screen.fill('black')
         screen.blit(text, text_rec)
+        screen.blit(not_active_text, (WIDTH - 900, 310))
         mpos = pygame.mouse.get_pos()
         pygame.draw.rect(screen, 'white', text_rec, 1)
         for event in pygame.event.get():
@@ -102,10 +135,16 @@ def start():
                     pygame.quit()
                     sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-               if text_rec.collidepoint(mpos):
-                   client.connect((HOST, PORT))
-                   conn()
-                   start_run = False
+                if text_rec.collidepoint(mpos):
+                    try:
+                        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        client.connect((HOST, PORT))
+                        connected = True
+                        rec = threading.Thread(target=receive, args=(client,))
+                        rec.start()
+                        conn(client)
+                    except:
+                        connected = False
 
         pygame.display.update()
 
--- multi/main.py
+++ multi/main.py
@@ -1,4 +1,3 @@
-import pygame
 import threading
 import socket
 
@@ -7,16 +6,49 @@ WIDTH = 1400
 
 
 HOST = '127.0.0.1'
-PORT = 9101
+PORT = 9102
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
-print("LISTENING..")
-client, addr = server.accept()
-print(f"CONNECTED WiTH {addr}")
-try:
-    msg = client.recv(1024).decode()
-    print(f"MESSAGE FROM {addr}: {msg}")
-except:
-    client.close()
+
+
+def receive(client, addr):
+    count = 0
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            if msg == '!D':
+                client.send(msg.encode())
+                client.close()
+                print(f"{addr} DISCONNECTED")
+                break
+            else:
+                print(f"MESSAGE FROM {addr}: {msg}")
+                count += 1
+        except:
+            client.send("!D".encode())
+            client.close()
+        if count >= 3:
+            client.send("!D".encode())
+            client.close()
+            print(f"{addr} DISCONNECTED")
+            break
+
+
+
+def start():
+    total = 0
+    while True:
+        print("LISTENING..")
+        client, addr = server.accept()
+        print(f"CONNECTED WiTH {addr}")
+
+        receive_thread = threading.Thread(target=receive, args=(client, addr))
+        receive_thread.start()
+        total += 1
+
+        if total >= 2:
+            break
+
+start()
