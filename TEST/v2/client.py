55f3bb6020d23cbe10830aaa9db4bb42d624e38a
sarthaksinghal@pm.me <sarthaksinghal@pm.me>
2023-05-22 18:22:37 +0530

    another cliect/server model

---
+++ TEST/v2/client.py
@@ -0,0 +1,150 @@
+import sys
+import threading
+
+import pygame
+
+import socket
+
+pygame.init()
+pygame.font.init()
+
+WIDTH = 1400
+HEIGHT = 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+font_ = pygame.font.SysFont('monospace', 50)
+to_connect_text = font_.render('CLICK TO CONNECT', True, 'white')
+to_connect_text_rec = to_connect_text.get_rect()
+connected_text = font_.render('CONNECTED!', True, 'white')
+connected_text_rec = connected_text.get_rect()
+starting_text = font_.render('STARTING GAME', True, 'white')
+starting_text_rec = starting_text.get_rect()
+kills_server = font_.render("KILL SERVER", True, 'white')
+kills_server_rec = kills_server.get_rect()
+
+
+
+
+class Network:
+
+    def __init__(self):
+        self.host = '127.0.0.1'
+        self.port = 9900
+        self.format = 'utf-8'
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.status = False
+        self.first_message = self.connect()
+
+
+    def connect(self):
+        try:
+            self.client.connect((self.host, self.port))
+            self.status = True
+            return self.client.recv(1024).decode(self.format)
+        except:
+            print("ERROR IN INITIAL CONNECTION")
+
+
+    def close(self):
+        try:
+            self.client.send("!D".encode(self.format))
+            self.client.close()
+            self.status = False
+            print("DISCONNECTED")
+        except:
+            print("SERVER DOWN")
+            self.status = False
+    def close_all(self):
+        try:
+            self.client.send("!SD".encode(self.format))
+            self.client.close()
+            self.status = False
+            print("DISCONNECTED")
+        except:
+            print("SERVER DOWN")
+            self.status = False
+
+    def send(self, msg):
+        try:
+            self.client.send(msg.encode(self.format))
+            return self.client.recv(1024).decode(self.format)
+        except:
+            print("ERROR IN FUNCTION 'Send' ")
+            self.status = False
+
+
+
+# Takes a touple and converts it into a string
+
+def make_msg(msg):
+    pass
+
+# Takes the string and convert it into touple
+
+def read_msg(msg):
+    pass
+
+
+
+
+
+
+
+def second():
+    Player = Network()
+    connection_status = Player.first_message
+    print(connection_status)
+
+    while Player.status:
+        screen.fill('black')
+        connected_text_rec.center = (WIDTH/2, HEIGHT/2)
+        screen.blit(connected_text, connected_text_rec)
+        starting_text_rec.center = (WIDTH/2, HEIGHT/2 + 60)
+        screen.blit(starting_text, starting_text_rec)
+        kills_server_rec.center = (WIDTH/2, HEIGHT/2 + 150)
+        screen.blit(kills_server, kills_server_rec)
+
+        mpos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    Player.close()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if kills_server_rec.collidepoint(mpos):
+                    Player.close_all()
+
+        pygame.display.update()
+
+
+def start():
+
+    while True:
+        screen.fill('black')
+        to_connect_text_rec.center = (WIDTH/2, HEIGHT/2)
+        screen.blit(to_connect_text, to_connect_text_rec)
+
+        mpos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if to_connect_text_rec.collidepoint(mpos):
+                    try:
+                        second()
+                    except:
+                        print("COULD NOT CONNECT..")
+
+
+
+        pygame.display.update()
+
+start()
---
+++ TEST/v2/server.py
@@ -0,0 +1,72 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9900
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+FORMAT = 'utf-8'
+
+clients = []
+
+
+# def read(string):
+#     string = string.split(',')
+#     return int(string[0]), int(string[1])
+#
+#
+# def make(tup):
+#     return str(tup[0]) + "," + str(tup[1])
+
+running = True
+
+def DISCONNECT(client, addr):
+    clients.remove(client)
+    print(f"{addr} DISCONNECTED")
+    # print("LISTENING...")
+
+
+
+def receive(client, addr):
+    global running
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+
+            if message == "!D":
+                print(message)
+                DISCONNECT(client, addr)
+                break
+            elif message == "!SD":
+                print(message)
+                print(f"Command to switch off server received {addr}")
+                DISCONNECT(client, addr)
+                running = False
+                break
+            else:
+                pass
+        except:
+            print(f"DISCONNECTED FROM {addr}")
+            clients.remove(client)
+            break
+
+
+
+
+
+while running:
+    print("LISTENING...")
+    client, addr = server.accept()
+    print(running)
+    if not running:
+        print("TURNING OFF SERVER..")
+        break
+    print(f"connected to {addr}")
+    clients.append(client)
+    client.send('200 status - CONNECTED'.encode(FORMAT))
+
+    client_thread = threading.Thread(target=receive, args=(client, addr))
+    client_thread.start()
